d7b4a3762a3cf054849bc2e109af2c9344abe8b3
Furkan Cabbar <ffcabbar@gmail.com>
2019-12-07 16:11:01 +0300

    Updates

---
+++ precache-manifest.5229168aae2279c64b085a01023bd5fd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "684409ef13b6d89688aa1ba3b4b2070a",
+    "url": "/react-head-or-tail/index.html"
+  },
+  {
+    "revision": "5a5bac110857ed3ba7af",
+    "url": "/react-head-or-tail/static/css/2.3caf6944.chunk.css"
+  },
+  {
+    "revision": "9f8437bd8b99ff45e978",
+    "url": "/react-head-or-tail/static/css/main.6f842b94.chunk.css"
+  },
+  {
+    "revision": "5a5bac110857ed3ba7af",
+    "url": "/react-head-or-tail/static/js/2.c39b9b5f.chunk.js"
+  },
+  {
+    "revision": "9f8437bd8b99ff45e978",
+    "url": "/react-head-or-tail/static/js/main.ebb3465e.chunk.js"
+  },
+  {
+    "revision": "80812439112a34121e81",
+    "url": "/react-head-or-tail/static/js/runtime~main.dcf31b9b.js"
+  },
+  {
+    "revision": "7c42669a738f1a1e15ef7ccedaaa8ed2",
+    "url": "/react-head-or-tail/static/media/1-lira-tura.7c42669a.png"
+  },
+  {
+    "revision": "9b70882eb706071cc877c4040c16702d",
+    "url": "/react-head-or-tail/static/media/1-lira-yazi.9b70882e.png"
+  }
+]);
